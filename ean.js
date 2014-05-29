d23117b7c70dac545ce3e68f57e51dd39bbd4944
josuevera <artujav@gmail.com>
2014-05-29 13:03:52 -0500

    Update ean.js

--- ean.js
+++ ean.js
@@ -1,32 +1,52 @@
 var request = require('request');
-
+ 
 var EAN = function() {};
-
+ 
 EAN.prototype.base_url = 'http://api.eancdn.com/ean-services/rs/hotel/v3/';
-
+ 
 EAN.prototype.list = function(params, callback) {
     request.get(this.base_url + 'list', {
         qs: params
     }, function(err, request, body) {
         var error = err;
         var result = null;
+        if (error) {
+            callback(null, {
+                HotelListResponse: {
+                    moreResultsAvailable: false,
+                    HotelList: {
+                        HotelSummary: []
+                    }
+                }
+            });
+            return;
+        }
         if (body == "<h1>503 Service Unavailable</h1>") {
             error = "Service Unavailable";
         } else if (body == "<h1>403 Developer Over Rate</h1>") {
             error = "403 Developer Over Rate";
         } else {
-            if(body) {
-                result = JSON.parse(body);
-                if (result['HotelListResponse'].hasOwnProperty('EanWsError')) {
-                    error = result['HotelListResponse']['EanWsError']['verboseMessage'];
-                    result = null;
+            result = JSON.parse(body);
+            if (result['HotelListResponse'].hasOwnProperty('EanWsError')) {
+                error = result['HotelListResponse']['EanWsError']['verboseMessage'];
+                result = null;
+                if (error == 'Results NULL') {
+                    error = null;
+                    result = {
+                        HotelListResponse: {
+                            moreResultsAvailable: false,
+                            HotelList: {
+                                HotelSummary: []
+                            }
+                        }
+                    };
                 }
             }
         }
         callback(error, result);
     });
 };
-
+ 
 EAN.prototype.info = function(params, callback) {
     request.get(this.base_url + 'info', {
         qs: params
@@ -47,7 +67,7 @@ EAN.prototype.info = function(params, callback) {
         callback(error, result);
     });
 };
-
+ 
 EAN.prototype.roomImages = function(params, callback) {
     request.get(this.base_url + 'roomImages', {
         qs: params
@@ -68,7 +88,7 @@ EAN.prototype.roomImages = function(params, callback) {
         callback(error, result);
     });
 };
-
+ 
 EAN.prototype.avail = function(params, callback) {
     request.get(this.base_url + 'avail', {
         qs: params
@@ -89,7 +109,7 @@ EAN.prototype.avail = function(params, callback) {
         callback(error, result);
     });
 };
-
+ 
 EAN.prototype.res = function(params, callback) {
     var booking_url = this.base_url.replace('http://api.eancdn.com', 'http://api.ean.com').replace('http://', 'https://book.');
     request.post(booking_url + 'res', {
@@ -99,5 +119,5 @@ EAN.prototype.res = function(params, callback) {
         callback(err, result);
     });
 };
-
+ 
 module.exports = new EAN();
